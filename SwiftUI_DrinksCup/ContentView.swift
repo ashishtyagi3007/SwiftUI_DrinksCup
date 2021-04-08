05dd35c7b3a8f8c4852046b968502d76bd8f0cee
Ashish Tyagi <ashisht@dewsolutions.in>
2021-04-08 20:30:14 +0530

    Initial Commit

---
+++ SwiftUI_DrinksCup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_DrinksCup
+//
+//  Created by Ashish Tyagi on 08/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
